e98c884c00ae4700dd1b85d2f5b46d47b9c5960b
luozhe73 <luozhe73@mySurface>
2019-05-14 22:36:47 +0800

    /

--- src/main/java/com/demo/dao/ProductCategoryDAO.java
+++ src/main/java/com/demo/dao/ProductCategoryDAO.java
@@ -1,9 +1,12 @@
 package com.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.dataObject.ProductCategory;
 
 public interface ProductCategoryDAO extends JpaRepository<ProductCategory, Integer>{
 
+	public List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 }
