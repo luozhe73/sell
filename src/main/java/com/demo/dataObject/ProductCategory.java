f7b053b99658be3faba6dd94f6f82615a2fe0370
luozhe73 <luozhe73@mySurface>
2019-05-14 22:32:23 +0800

    !

--- src/main/java/com/demo/dataObject/ProductCategory.java
+++ src/main/java/com/demo/dataObject/ProductCategory.java
@@ -9,8 +9,11 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.DynamicUpdate;
 
+import lombok.Data;
+
 @Entity
 @DynamicUpdate
+@Data
 public class ProductCategory {
 
 	@Id
@@ -24,52 +27,20 @@ public class ProductCategory {
 	private Date createTime;
 	
 	private Date updateTime;
-	
-	public Integer getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(Integer categoryId) {
-		this.categoryId = categoryId;
-	}
-
-	public String getCategoryName() {
-		return categoryName;
-	}
-
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
-
-	public Integer getCategoryType() {
-		return categoryType;
-	}
-
-	public void setCategoryType(Integer categoryType) {
-		this.categoryType = categoryType;
-	}
-
-	
-	public Date getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-
-	public Date getUpdateTime() {
-		return updateTime;
-	}
-
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
 
 	@Override
 	public String toString() {
 		return "ProductCategory [categoryId=" + categoryId + ", categoryName=" + categoryName + ", categoryType="
 				+ categoryType + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
+	}
+
+	public ProductCategory(Integer categoryId, String categoryName, Integer categoryType, Date createTime,
+			Date updateTime) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		this.categoryType = categoryType;
+		this.createTime = createTime;
+		this.updateTime = updateTime;
 	}
 
 }
