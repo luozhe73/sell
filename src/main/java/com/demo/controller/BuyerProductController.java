bda9dd2fe9840351dd92b2f69f3651d6d6f95051
luozhe73 <1043269439@qq.com>
2019-05-16 22:40:56 +0800

    拼装result

--- src/main/java/com/demo/controller/BuyerProductController.java
+++ src/main/java/com/demo/controller/BuyerProductController.java
@@ -3,10 +3,17 @@ package com.demo.controller;
 import com.demo.VO.ProductInfoVO;
 import com.demo.VO.ProductVO;
 import com.demo.VO.ResultVO;
+import com.demo.dataObject.ProductCategory;
+import com.demo.dataObject.ProductInfo;
+import com.demo.service.CategoryService;
+import com.demo.service.ProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,19 +21,55 @@ import java.util.List;
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/list")
-    public ResultVO list(){
+    public ResultVO list() {
+
+        //1.查询所有上架商品
+        List<ProductInfo> productInfoList=productService.findUpAll();
+
+        //2.查询类目(一次性查询)
+        List<Integer> categoryTypeList = new ArrayList<Integer>();
+
+        for (ProductInfo productInfo:productInfoList
+             ) {
+            categoryTypeList.add(productInfo.getCategoryType());
+        }
+        List<ProductCategory> categoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+
+        //3.数据拼装
+        List<ProductVO> productVOList=new ArrayList<>();
+        for (ProductCategory productcategory:categoryList
+             ) {
+            ProductVO productVO=new ProductVO();
+            productVO.setCategoryType(productcategory.getCategoryType());
+            productVO.setCategoryName(productcategory.getCategoryName());
+
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo:productInfoList
+                 ) {
+                if(productInfo.getCategoryType().equals(productcategory.getCategoryType())){
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo,productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
 
         ResultVO resultVO = new ResultVO();
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO=new ProductInfoVO();
 
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-
-        resultVO.setData(Arrays.asList(productVO));
+        resultVO.setData(productVOList);
         resultVO.setCode(0);
         resultVO.setMsg("success");
 
-        return  resultVO;
+        return resultVO;
     }
 }
---
+++ src/main/java/com/demo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,39 @@
+package com.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.demo.dao.ProductInfoDAO;
+import com.demo.dataObject.ProductInfo;
+import com.demo.enums.ProductStatusEnum;
+import com.demo.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired
+	private ProductInfoDAO dao;
+	
+	@Override
+	public ProductInfo findOne(String productId) {
+		return dao.findById(productId).get();
+	}
+
+	@Override
+	public List<ProductInfo> findUpAll() {
+		return dao.findByProductStatus(ProductStatusEnum.UP.getCode());
+	}
+
+	@Override
+	public Page<ProductInfo> findAll(Pageable pageable) {
+		return dao.findAll(pageable);
+	}
+
+	@Override
+	public ProductInfo save(ProductInfo productInfo) {
+		return dao.save(productInfo);
+	}
+
+}
