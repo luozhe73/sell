a55be0075c8afb5c6862543ccdbe4306121be6b5
luozhe73 <1043269439@qq.com>
2019-05-16 22:11:56 +0800

    vo

---
+++ src/main/java/com/demo/VO/ProductVO.java
@@ -0,0 +1,19 @@
+package com.demo.VO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductVO {
+
+    @JsonProperty("name")
+    private String categoryName;
+
+    @JsonProperty("type")
+    private String categoryType;
+
+    @JsonProperty("foods")
+    private List<ProductInfoVO> productInfoVOList;
+}
--- src/main/java/com/demo/controller/BuyerProductController.java
+++ src/main/java/com/demo/controller/BuyerProductController.java
@@ -1,10 +1,13 @@
 package com.demo.controller;
 
+import com.demo.VO.ProductInfoVO;
+import com.demo.VO.ProductVO;
 import com.demo.VO.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -15,8 +18,15 @@ public class BuyerProductController {
     public ResultVO list(){
 
         ResultVO resultVO = new ResultVO();
+        ProductVO productVO = new ProductVO();
+        ProductInfoVO productInfoVO=new ProductInfoVO();
+
+        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
+
+        resultVO.setData(Arrays.asList(productVO));
         resultVO.setCode(0);
-        resultVO.setMsg("error");
+        resultVO.setMsg("success");
+
         return  resultVO;
     }
 }
